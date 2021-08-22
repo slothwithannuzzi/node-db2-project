0ae72dfdee1ee51d467295542f1bf21ed5ace1dc
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2021-08-22 10:23:47 -0500

    wrote migration file

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.table("cars", tbl =>{
+    tbl.increments();
+    tbl.string("vin").unique().notNullable();
+    tbl.string("make").notNullable();
+    tbl.string("model").notNullable();
+    tbl.decimal("mileage").notNullable();
+    tbl.string("title");
+    tbl.string("transmission");
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists("cars");
 };
